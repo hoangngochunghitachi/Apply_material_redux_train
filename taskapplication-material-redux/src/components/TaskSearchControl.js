e24e07767b47a369e3c3fe5041f55d287fe2786f
Hung Ngoc Hoang <hungnh@hitachiconsulting.com>
2020-11-02 17:34:54 +0700

    update title

--- taskapplication-material-redux/src/components/TaskSearchControl.js
+++ taskapplication-material-redux/src/components/TaskSearchControl.js
@@ -32,7 +32,7 @@ class TaskSearchControl extends Component {
         const { classes } = this.props;
         return (
             <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-                <TextField label={<Typography className={classes.lblNote}> Type keyword search </Typography>}
+                <TextField label={<Typography className={classes.lblNote}>Keyword</Typography>}
                     className={classes.textSearchControl} name="keyword" value={keyword} onChange={this.onHandleChange}
                     InputProps={{ classes: { input: classes.textField } }, { endAdornment: (<IconButton onClick={this.onSearch}> <SearchIcon className={classes.iconSearch} /></IconButton>) }} />
 
